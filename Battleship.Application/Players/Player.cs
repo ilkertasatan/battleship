f8503b893a8b874a89fd691da8049c1345ecbe72
ilkertasatan <itasatan@gmail.com>
2021-02-16 15:51:10 +0100

    Implement Player and tests

---
+++ Battleship.Application/Players/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Battleship.Application.Extensions;
+using Battleship.Application.Games;
+
+namespace Battleship.Application.Players
+{
+    public class Player : IPlayer
+    {
+        public Player(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+
+            Name = name;
+            GameBoard = new GameBoard(
+                BoardSize.Row,
+                BoardSize.Column);
+        }
+
+        public string Name { get; }
+        public IGameBoard GameBoard { get; set; }
+
+        public ShotResult FireShot(Coordinate coordinate)
+        {
+            var grid = GameBoard.Grids.First(g => g.Coordinate.Row == coordinate.Row &&
+                                                  g.Coordinate.Column == coordinate.Column);
+
+            if (!grid.IsShipPlacedOn)
+            {
+                grid.GridType = GridType.Miss;
+                return ShotResult.Miss;
+            }
+
+            var ship = GameBoard.Ships.First(s => s.ShipId == grid.ShipId);
+
+            ship.IncreaseHit();
+            grid.GridType = GridType.Hit;
+
+            return ship.IsSunk() ? ShotResult.Sink : ShotResult.Hit;
+        }
+    }
+}
---
+++ Battleship.Tests/PlayerTests.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Battleship.Application;
+using Battleship.Application.Games;
+using Battleship.Application.Players;
+using Battleship.Application.Ships;
+using FluentAssertions;
+using Xunit;
+
+namespace Battleship.Tests
+{
+    public class PlayerTests
+    {
+        private IPlayer _sut;
+        
+        [Fact]
+        public void Should_Return_Hit_Result_When_A_Ship_Is_Hit()
+        {
+            var expectedGame = GivenGame();
+            var expectedGrid = expectedGame.Player.GameBoard.Grids.First(g => g.IsShipPlacedOn);
+            _sut = expectedGame.Player;
+
+            var actualResult = _sut.FireShot(expectedGrid.Coordinate);
+
+            actualResult.Should().BeOfType<ShotResult>().Which.Should().Be(ShotResult.Hit);
+        }
+        
+        [Fact]
+        public void Should_Return_Miss_Result_When_Ship_Is_Missed()
+        {
+            var expectedGame = GivenGame();
+            var expectedGrid = expectedGame.Player.GameBoard.Grids.First(g => !g.IsShipPlacedOn);
+            _sut = expectedGame.Player;
+
+            var actualResult = _sut.FireShot(expectedGrid.Coordinate);
+
+            actualResult.Should().BeOfType<ShotResult>().Which.Should().Be(ShotResult.Miss);
+        }
+        
+        [Fact]
+        public void Should_Return_Sink_Result_When_A_Ship_Is_Sunk()
+        {
+            var expectedGame = GivenGame();
+            var expectedShip = new BattleShip
+            {
+                Hits = 4
+            };
+            expectedGame.Player.GameBoard.Ships = new List<IShip>
+            {
+                expectedShip
+            };
+            expectedGame.Player.GameBoard.Grids = new List<IGrid>
+            {
+                new Grid(1, 1)
+                {
+                    GridType = GridType.Battleship,
+                    ShipId = expectedShip.ShipId
+                }
+            };
+            _sut = expectedGame.Player;
+
+            var actualResult = _sut.FireShot(new Coordinate(1, 1));
+
+            actualResult.Should().BeOfType<ShotResult>().Which.Should().Be(ShotResult.Sink);
+        }
+
+        private static Game GivenGame()
+        {
+            var game = new Game(new Player("player"));
+            game.Start();
+
+            return game;
+        }
+    }
+}
