2ee97ea3320a4f8fcaf437be1dc4eb642fb54c52
ilkertasatan <itasatan@gmail.com>
2021-02-16 15:52:14 +0100

    Add Coordinate object

---
+++ Battleship.Application/Coordinate.cs
@@ -0,0 +1,20 @@
+﻿namespace Battleship.Application
+{
+    public class Coordinate
+    {
+        public static Coordinate Empty = new Coordinate();
+
+        private Coordinate()
+        {
+        }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public int Row { get; }
+        public int Column { get; }
+    }
+}
