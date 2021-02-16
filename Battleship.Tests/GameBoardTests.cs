1f37a22afccc22f043f9a5ba2547edf1bd016fb1
ilkertasatan <itasatan@gmail.com>
2021-02-16 15:51:36 +0100

    Add game board tests

---
+++ Battleship.Tests/GameBoardTests.cs
@@ -0,0 +1,20 @@
+﻿using Battleship.Application.Games;
+using FluentAssertions;
+using Xunit;
+
+namespace Battleship.Tests
+{
+    public class GameBoardTests
+    {
+        [Fact]
+        public void Should_Create_New_Board_Given_Row_And_Column()
+        {
+            const int expectedRow = 10;
+            const int expectedColumn = 10;
+            var sut = new GameBoard(expectedRow, expectedColumn);
+
+            sut.Row.Should().Be(expectedRow);
+            sut.Column.Should().Be(expectedColumn);
+        }
+    }
+}
