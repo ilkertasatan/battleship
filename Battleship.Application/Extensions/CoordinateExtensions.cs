a5f6663bb3ab527dcf12fd65ed50926dcaaa949b
ilkertasatan <itasatan@gmail.com>
2021-02-16 15:53:11 +0100

    Add Coordinate extensions

---
+++ Battleship.Application/Extensions/CoordinateExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Battleship.Application.Extensions
+{
+    public static class CoordinateExtensions
+    {
+        public static bool IsValid(this Coordinate coordinate)
+        {
+            return coordinate.Row > 0 &&
+                   coordinate.Column > 0;
+        }
+    }
+}
