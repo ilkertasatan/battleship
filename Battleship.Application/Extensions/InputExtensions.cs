85866be85fcc0cddb2bf13f8dc17014017e60eca
ilkertasatan <itasatan@gmail.com>
2021-02-16 15:52:47 +0100

    Add Input extensions and tests

---
+++ Battleship.Application/Extensions/InputExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Battleship.Application.Extensions
+{
+    public static class InputExtensions
+    {
+        public static Coordinate ToCoordinate(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input) || input.Length < 2)
+                return Coordinate.Empty;
+            
+            var chars = input.ToCharArray();
+            var x = chars[0].ToX();
+            int.TryParse(chars[1].ToString(), out var y);
+
+            return new Coordinate(x, y);
+        }
+
+        private static int ToX(this char c)
+        {
+            return c switch
+            {
+                'A' => 1,
+                'B' => 2,
+                'C' => 3,
+                'D' => 4,
+                'E' => 5,
+                'F' => 6,
+                'G' => 7,
+                'H' => 8,
+                'I' => 9,
+                'J' => 10,
+                _ => 0
+            };
+        }
+    }
+}
---
+++ Battleship.Tests/InputExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using Battleship.Application;
+using Battleship.Application.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Battleship.Tests
+{
+    public class InputExtensionsTests
+    {
+        [Theory]
+        [InlineData("A3", 1, 3)]
+        [InlineData("B7", 2, 7)]
+        [InlineData("C8", 3, 8)]
+        [InlineData("D6", 4, 6)]
+        [InlineData("E1", 5, 1)]
+        [InlineData("F2", 6, 2)]
+        [InlineData("G5", 7, 5)]
+        [InlineData("H6", 8, 6)]
+        [InlineData("I7", 9, 7)]
+        [InlineData("J9", 10, 9)]
+        public void Should_Convert_Coordinate_Given_Valid_Input(string expectedInput, int expectedX, int expectedY)
+        {
+            var expectedResult = new Coordinate(expectedX, expectedY);
+
+            var actualResult = expectedInput.ToCoordinate();
+
+            actualResult.IsValid().Should().BeTrue();
+            actualResult.Should().BeEquivalentTo(expectedResult);
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("invalid-input")]
+        [InlineData("N1")]
+        [InlineData("Z99")]
+        public void Should_Not_Convert_Coordinate_Given_Invalid_Input(string expectedInput)
+        {
+            var actualResult = expectedInput.ToCoordinate().IsValid();
+
+            actualResult.Should().BeFalse();
+        }
+    }
+}
